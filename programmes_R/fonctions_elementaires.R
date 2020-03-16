d1a1789ddfaae9ed8f4d71986e33e7461b309b36
NPaliod <62190030+NPaliod@users.noreply.github.com>
2020-03-16 22:12:52 +0100

    modif_fctns_elementaires
    Ajout de la possibilité de ne pas avoir 0 porteur sain au début du jour 0 dans la fonctions contamination
    Allègement d'une vérification d'égalité dans la fonction évolution (permet de ne pas être bloquant en cas d'arrondis)

--- programmes_R/fonctions_elementaires.R
+++ programmes_R/fonctions_elementaires.R
@@ -1,6 +1,5 @@
-# Programme des fonctions contamination et Ã©volution
+# Programme des fonctions contamination et évolution
 
-# DonnÃ©es de test
 pop_test_1_1 <- list(
   non_conta = 1000
   , sains = setNames(rep(0,14), c(0:13))
@@ -32,28 +31,28 @@ pop_test_2 <- list(
   )
 
 
-cluster_test <- setNames(list(pop_test_1_1, pop_test_1_2) , c("rÃ©sistant", "vulnÃ©rable"))
+cluster_test <- setNames(list(pop_test_1_1, pop_test_1_2) , c("résistant", "vulnérable"))
 
 # Fonction contamination
 contamination <- function(pop, cluster, contag, prob_sain, prob_contact, n_contact){
   
-  # pop <- "vulnÃ©rable"; cluster = cluster_test; prob_sain = 0.9; prob_contact = setNames(c(0.8, 0.2), c("vulnÃ©rable", "rÃ©sistant")); n_contact = 20; contag = 0.005
-  # pop <- "rÃ©sistant"; cluster = cluster_test; prob_sain = 0.99; prob_contact = setNames(c(0.2, 0.8), c("vulnÃ©rable", "rÃ©sistant")); n_contact = 100; contag = 0.005
+  # pop <- "vulnérable"; cluster = cluster_test; prob_sain = 0.9; prob_contact = setNames(c(0.8, 0.2), c("vulnérable", "résistant")); n_contact = 20; contag = 0.005
+  # pop <- "résistant"; cluster = cluster_test; prob_sain = 0.99; prob_contact = setNames(c(0.2, 0.8), c("vulnérable", "résistant")); n_contact = 100; contag = 0.005
   
   
-  # Etape 1 : VÃ©rifications
-  if(typeof(cluster) != "list") stop("Le cluster doit Ãªtre une liste de populations")
+  # Etape 1 : Vérifications
+  if(typeof(cluster) != "list") stop("Le cluster doit être une liste de populations")
   if(is.null(cluster[[pop]])) stop ("Cette population n'existe pas dans le cluster")
-  if(is.null(names(prob_contact))) stop ("La probabilitÃ© de contact doit Ãªtre un vecteur nommÃ©")
-  if(any(!names(prob_contact) %in% names(cluster))) stop("La probabilitÃ© de contact doit Ãªtre un vecteur nommÃ© dont les noms sont les populations du cluster")
+  if(is.null(names(prob_contact))) stop ("La probabilité de contact doit être un vecteur nommé")
+  if(any(!names(prob_contact) %in% names(cluster))) stop("La probabilité de contact doit être un vecteur nommé dont les noms sont les populations du cluster")
   
   
-  # Etape 2 : Calcul de la probabilitÃ© pour un individu d'Ãªtre contaminÃ©
+  # Etape 2 : Calcul de la probabilité pour un individu d'être contaminé
   
   # Nombre de contacts avec des individus de chaque population
   n_contact_pop <- n_contact * prob_contact
   
-  # Nombre de contacts avec des individus contaminÃ©s par individu de la population
+  # Nombre de contacts avec des individus contaminés par individu de la population
   n_contact_contam <- sum(
     sapply(names(n_contact_pop), function(nom_pop){
       popul <- cluster[[nom_pop]]
@@ -61,19 +60,19 @@ contamination <- function(pop, cluster, contag, prob_sain, prob_contact, n_conta
     })
   )
   
-  # ProbabilitÃ© d'Ãªtre contaminÃ© pour un individu
+  # Probabilité d'être contaminé pour un individu
   prob_contam <- 1 - (1 - contag)^n_contact_contam
   
-  # Etape 3 : Nombre d'individus contaminÃ©s dans la population et de chaque type
+  # Etape 3 : Nombre d'individus contaminés dans la population et de chaque type
   
-  # Nombre d'individus contaminÃ©s
+  # Nombre d'individus contaminés
   population <- cluster[[pop]]
   n_contam <- population[["non_conta"]] * prob_contam
   
   # Nombre d'individus devenant porteurs sains
   population[["non_conta"]] <- population[["non_conta"]] - n_contam
-  population[["sains"]]["0"] <- n_contam * prob_sain
-  population[["non_decl"]]["0"] <- n_contam * (1 - prob_sain)
+  population[["sains"]]["0"] <- population[["sains"]]["0"] + n_contam * prob_sain
+  population[["non_decl"]]["0"] <- population[["non_decl"]]["0"] + n_contam * (1 - prob_sain)
   
   return(population)
 }
@@ -85,13 +84,13 @@ evolution <- function(pop, d, g, prob_grav, satur_hop, tx_mort){
   
   taille_pop <- sum(sapply(pop, sum))
   
-  # Etape 1 : VÃ©rifications
+  # Etape 1 : Vérifications
   
-  if(length(pop) != 7) stop("L'objet pop doit contenir 7 Ã©lÃ©ments dÃ©crivant la population")
-  if(d >= g) stop("La durÃ©e d avant que la maladie se dÃ©clare doit Ãªtre strictement infÃ©rieure Ã  la durÃ©e g Ã  laquelle elle devient grave")
-  if(length(tx_mort) != 2) stop ("Le taux de mortalitÃ© doit contenir 2 Ã©lÃ©ments : 1 en cas de saturation des hÃ´pitaux, l'autre en cas de fonctionnement normal")
-  if(any(!names(tx_mort) %in% c("satur", "normal"))) stop("Les noms de l'objet taux de mortalitÃ© sont incorrects")
-  if(! satur_hop %in% c(0, 1)) stop("La saturation des hÃ´pitaux doit Ãªtre une indicatrice")
+  if(length(pop) != 7) stop("L'objet pop doit contenir 7 éléments décrivant la population")
+  if(d >= g) stop("La durée d avant que la maladie se déclare doit être strictement inférieure à la durée g à laquelle elle devient grave")
+  if(length(tx_mort) != 2) stop ("Le taux de mortalité doit contenir 2 éléments : 1 en cas de saturation des hôpitaux, l'autre en cas de fonctionnement normal")
+  if(any(!names(tx_mort) %in% c("satur", "normal"))) stop("Les noms de l'objet taux de mortalité sont incorrects")
+  if(! satur_hop %in% c(0, 1)) stop("La saturation des hôpitaux doit être une indicatrice")
   
   # Etape 2 : Evolution des porteurs sains
   pop[["immu"]] <- pop[["immu"]] + pop[["sains"]][length(pop[["sains"]])]
@@ -111,7 +110,7 @@ evolution <- function(pop, d, g, prob_grav, satur_hop, tx_mort){
   pop[["grav"]][as.character(g+1)] <- setNames(pop[["decl"]][as.character(g)] * prob_grav, as.character(g+1))
   
   
-  # Etape 4 : Evolution des malades dÃ©clarÃ©s
+  # Etape 4 : Evolution des malades déclarés
   pop[["immu"]] <- pop[["immu"]] + pop[["decl"]][length(pop[["decl"]])]
   pop[["decl"]] <- setNames(
     c(rep(0, d+1)
@@ -122,42 +121,43 @@ evolution <- function(pop, d, g, prob_grav, satur_hop, tx_mort){
     ,   names(pop[["decl"]])
   )
   
-  # Etape 5 : Evolution des malades non dÃ©clarÃ©s
+  # Etape 5 : Evolution des malades non déclarés
   pop[["non_decl"]] <- setNames(c(0, pop[["non_decl"]][1 : d], rep(0, length(pop[["non_decl"]] ) - d - 1) ), names(pop[["non_decl"]]))
   
   # Etape 6 : mise en forme
   names(pop[["mort"]]) <- NULL
   names(pop[["immu"]]) <- NULL
-  
-  # Etape 7 : VÃ©rification
-  if(sum(sapply(pop, sum)) != taille_pop) stop ("La taille de la population en entrÃ©e est diffÃ©rente de celle en sortie")
+  spy <<- taille_pop
+  spy2 <<- sum(sapply(pop, sum)) 
+  # Etape 7 : Vérification
+  if(abs(sum(sapply(pop, sum)) - taille_pop) > 10^-8) stop ("La taille de la population en entrée est différente de celle en sortie")
   
   # Etape 8 : Sortie
   return(pop)
 }
 
 # library(microbenchmark)
-# microbenchmark(contamination(pop  ="vulnÃ©rable", cluster = cluster_test, prob_sain = 0.9, prob_contact = setNames(c(0.8, 0.2), c("vulnÃ©rable", "rÃ©sistant")), n_contact = 20, contag = 0.005), times = 10000)
-# moyenne : environ 100 micro secondes => 100 000 : environ 10 secondes pour faire la contamination sur France entiÃ¨re, sur une population
+# microbenchmark(contamination(pop  ="vulnérable", cluster = cluster_test, prob_sain = 0.9, prob_contact = setNames(c(0.8, 0.2), c("vulnérable", "résistant")), n_contact = 20, contag = 0.005), times = 10000)
+# moyenne : environ 100 micro secondes => 100 000 : environ 10 secondes pour faire la contamination sur France entière, sur une population
 # microbenchmark(evolution(pop = pop_test_2, d = 4, g = 7, prob_grav = 0.4 , tx_mort = setNames(c(0.9, 0.2), c("satur", "normal")) , satur_hop = 0), times = 10000)
-# moyenne : environ 140 micro secondes => 40000 : environ 6 secondes pour faire la contamination sur France entiÃ¨re, sur une population
+# moyenne : environ 140 micro secondes => 40000 : environ 6 secondes pour faire la contamination sur France entière, sur une population
 
 
 contamination_cluster <- function(cluster, contag, v_prob_sain, mat_prob_contact, v_contact){
   
-  # cluster = cluster_test; contag = 0.005; v_prob_sain = setNames(c(0.9, 0.99), c("vulnÃ©rable", "rÃ©sistant")); v_contact = setNames(c(20, 100), c("vulnÃ©rable", "rÃ©sistant")); mat_prob_contact = rbind(c(0.5, 0.5), c(0.1, 0.9)); row.names(mat_prob_contact) = c("vulnÃ©rable", "rÃ©sistant"); colnames(mat_prob_contact) = c("vulnÃ©rable", "rÃ©sistant");
-  # cluster = cluster_test; contag = 0.005; v_prob_sain = setNames(c(0.9, 0.99), c("vulnÃ©rable", "rÃ©sistant")); v_contact = setNames(c(20, 100), c("vulnÃ©rable", "rÃ©sistant")); mat_prob_contact = rbind(c(0.8, 0.2), c(0.2, 0.8)); row.names(mat_prob_contact) = c("vulnÃ©rable", "rÃ©sistant"); colnames(mat_prob_contact) = c("vulnÃ©rable", "rÃ©sistant");
+  # cluster = cluster_test; contag = 0.005; v_prob_sain = setNames(c(0.9, 0.99), c("vulnérable", "résistant")); v_contact = setNames(c(20, 100), c("vulnérable", "résistant")); mat_prob_contact = rbind(c(0.5, 0.5), c(0.1, 0.9)); row.names(mat_prob_contact) = c("vulnérable", "résistant"); colnames(mat_prob_contact) = c("vulnérable", "résistant");
+  # cluster = cluster_test; contag = 0.005; v_prob_sain = setNames(c(0.9, 0.99), c("vulnérable", "résistant")); v_contact = setNames(c(20, 100), c("vulnérable", "résistant")); mat_prob_contact = rbind(c(0.8, 0.2), c(0.2, 0.8)); row.names(mat_prob_contact) = c("vulnérable", "résistant"); colnames(mat_prob_contact) = c("vulnérable", "résistant");
   
-  # Etape 1 : VÃ©rifications
+  # Etape 1 : Vérifications
   
-  if(is.null(names(cluster))) stop ("Les populations du cluster doivent Ãªtre nommÃ©es")
-  if(length(unique(length(v_prob_sain), length(v_contact), length(cluster), nrow(mat_prob_contact), ncol(mat_prob_contact))) != 1) stop("Les objets en entrÃ©e n'ont pas tous la mÃªme longueur : certaines populations n'ont pas de paramÃ¨tres ou certains paramÃ¨tres s'appliquent Ã  des populations qui n'existent pas")
-  if(any(! names(cluster) %in% names(v_contact)) | any(! names(cluster) %in% names(v_prob_sain)) | any(! names(cluster) %in% row.names(mat_prob_contact)) | any(! names(cluster) %in% colnames(mat_prob_contact))) stop("Les paramÃ¨tres ne sont pas nommÃ©s comme la population")
-  if(any(apply(mat_prob_contact, 1, sum) != 1)) stop("Les lignes de mat_prob_contact doivent sommer Ã  1")
+  if(is.null(names(cluster))) stop ("Les populations du cluster doivent être nommées")
+  if(length(unique(length(v_prob_sain), length(v_contact), length(cluster), nrow(mat_prob_contact), ncol(mat_prob_contact))) != 1) stop("Les objets en entrée n'ont pas tous la même longueur : certaines populations n'ont pas de paramètres ou certains paramètres s'appliquent à des populations qui n'existent pas")
+  if(any(! names(cluster) %in% names(v_contact)) | any(! names(cluster) %in% names(v_prob_sain)) | any(! names(cluster) %in% row.names(mat_prob_contact)) | any(! names(cluster) %in% colnames(mat_prob_contact))) stop("Les paramètres ne sont pas nommés comme la population")
+  if(any(apply(mat_prob_contact, 1, sum) != 1)) stop("Les lignes de mat_prob_contact doivent sommer à 1")
   
-  # Etape 2 : Application de la fonction contamination Ã  chaque population
+  # Etape 2 : Application de la fonction contamination à chaque population
   cluster_contamine <- lapply(setNames(names(cluster), names(cluster)), function(nom_pop){
-    # nom_pop = "vulnÃ©rable"
+    # nom_pop = "vulnérable"
     population <- cluster[[nom_pop]]
     contamination(pop = nom_pop, cluster = cluster, contag = contag, prob_sain = v_prob_sain[nom_pop], prob_contact = mat_prob_contact[nom_pop, ], n_contact = v_contact[nom_pop])
   })
@@ -167,30 +167,31 @@ contamination_cluster <- function(cluster, contag, v_prob_sain, mat_prob_contact
 }
 
 # mat_prob_contact <- rbind(c(0.5, 0.5), c(0.1, 0.9))
-# row.names(mat_prob_contact) <- c("vulnÃ©rable", "rÃ©sistant")
-# colnames(mat_prob_contact) <- c("vulnÃ©rable", "rÃ©sistant")
+# row.names(mat_prob_contact) <- c("vulnérable", "résistant")
+# colnames(mat_prob_contact) <- c("vulnérable", "résistant")
 
-# microbenchmark(contamination_cluster(cluster = cluster_test, contag = 0.005, v_prob_sain = setNames(c(0.9, 0.99), c("vulnÃ©rable", "rÃ©sistant")), v_contact = setNames(c(20, 100), c("vulnÃ©rable", "rÃ©sistant")), mat_prob_contact = mat_prob_contact), times = 10000)
-# moyenne : environ 240 micro secondes => 100 000 : environ 24 secondes pour faire la contamination sur France entiÃ¨re, sur une population
+# microbenchmark(contamination_cluster(cluster = cluster_test, contag = 0.005, v_prob_sain = setNames(c(0.9, 0.99), c("vulnérable", "résistant")), v_contact = setNames(c(20, 100), c("vulnérable", "résistant")), mat_prob_contact = mat_prob_contact), times = 10000)
+# moyenne : environ 240 micro secondes => 100 000 : environ 24 secondes pour faire la contamination sur France entière, sur une population
 
 
 
 evolutions_cluster <- function(cluster, d, g, v_prob_grav, satur_hop, mat_tx_mort){
   
-  # cluster = list(pop_test_2, pop_test_2); names(cluster) = c("vulnÃ©rable", "rÃ©sistant"); d = 4; g = 7; satur_hop = 0; v_prob_grav = setNames(c(0.4, 0.1), c("vulnÃ©rable", "rÃ©sistant")); mat_tx_mort = rbind(c(0.9, 0.2), c(0.1, 0.01)); colnames(mat_tx_mort) = c("satur", "normal"); row.names(mat_tx_mort) = c("vulnÃ©rable", "rÃ©sistant")
+  # cluster = list(pop_test_2, pop_test_2); names(cluster) = c("vulnérable", "résistant"); d = 4; g = 7; satur_hop = 0; v_prob_grav = setNames(c(0.4, 0.1), c("vulnérable", "résistant")); mat_tx_mort = rbind(c(0.9, 0.2), c(0.1, 0.01)); colnames(mat_tx_mort) = c("satur", "normal"); row.names(mat_tx_mort) = c("vulnérable", "résistant")
 
-  # Etape 1 : VÃ©rifications
+  # Etape 1 : Vérifications
   
-  if(is.null(names(cluster))) stop ("Les populations du cluster doivent Ãªtre nommÃ©es")
-  if(length(unique(length(v_prob_grav), length(cluster), nrow(mat_tx_mort))) != 1) stop("Les objets en entrÃ©e n'ont pas tous la mÃªme longueur : certaines populations n'ont pas de paramÃ¨tres ou certains paramÃ¨tres s'appliquent Ã  des populations qui n'existent pas")
-  if(any(! names(cluster) %in% names(v_prob_grav)) | any(! names(cluster) %in% row.names(mat_tx_mort)) ) stop("Les paramÃ¨tres ne sont pas nommÃ©s comme la population")
+  if(is.null(names(cluster))) stop ("Les populations du cluster doivent être nommées")
+  if(length(unique(length(v_prob_grav), length(cluster), nrow(mat_tx_mort))) != 1) stop("Les objets en entrée n'ont pas tous la même longueur : certaines populations n'ont pas de paramètres ou certains paramètres s'appliquent à des populations qui n'existent pas")
+  if(any(! names(cluster) %in% names(v_prob_grav)) | any(! names(cluster) %in% row.names(mat_tx_mort)) ) stop("Les paramètres ne sont pas nommés comme la population")
 
-  # Etape 2 : Application de la fonction contamination Ã  chaque population
+  # Etape 2 : Application de la fonction contamination à chaque population
   cluster_j_plus_1 <- lapply(setNames(names(cluster), names(cluster)), function(nom_pop){
-    # nom_pop = "vulnÃ©rable"
+    # nom_pop = "vulnérable"
     evolution(pop = cluster[[nom_pop]], d = d, g = g, prob_grav = v_prob_grav[[nom_pop]], tx_mort = mat_tx_mort[nom_pop, ], satur_hop = satur_hop)
   })
   
   return(cluster_j_plus_1)
   
 }
+
